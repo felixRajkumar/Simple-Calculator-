75c6f91b915c82c2de1f74150dde6c3fb5f7fda7
felix.ar <felix.ar@zohocorp.com>
2021-10-31 13:47:07 +0530

    Simple addition calculator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,25 @@
 const express = require("express");
-
+const parser = require("body-parser");
 const app = express();
 
 const port =3000;
+app.use(parser.urlencoded({extended: true}));
+
 
 app.get('/', (req, resp) => {
-    resp.write("Hello World");
-    resp.send();
+    resp.sendFile(__dirname+"/index.html");
 });
+
+app.post('/', (req, resp) => {
+    var num1 = Number(req.body.num1);
+
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    resp.send("The result is "+ result);
+});
+
 
 app.listen(3000, () => {
     console.log('Listening on port ' + port);
